b0a689ad43d6056bbf0d07f53beadb50f8ff0d81
aehaaland2-lab <aehaaland2@gmail.com>
2026-08-21 18:53:11 +0500

    Fix auth

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import EditProfile from "./pages/EditProfile"
 import TestEditor from "./pages/TestEditor"
 import PlayTest from "./pages/PlayTest"
 import { supabase } from "./supabase"
-import CompleteProfile from "./pages/CompleteProfile"
 
 import Layout from "./components/Layout"
 
@@ -20,16 +19,10 @@ function Messages() {
 
 export default function App() {
   const [user, setUser] = useState(null)
-  const [profileExists, setProfileExists] = useState(null)
   const [loading, setLoading] = useState(true)
 
   const logout = async () => {
     await supabase.auth.signOut()
-  }
-  async function checkProfile(user) {
-  if (!user) {
-    setProfileExists(null)
-    return
   }
 
   const { data, error } = await supabase
@@ -52,8 +45,7 @@ setProfileExists(!!data)
       const currentUser = data.session?.user || null
 
 setUser(currentUser)
-checkProfile(currentUser)
-      setLoading(false)
+setLoading(false)
     })
 
     const {
@@ -62,7 +54,6 @@ checkProfile(currentUser)
       const currentUser = session?.user || null
 
 setUser(currentUser)
-checkProfile(currentUser)
     })
 
     return () => subscription.unsubscribe()
@@ -81,24 +72,22 @@ checkProfile(currentUser)
       <Route path="/edit-profile" element={<EditProfile />} />
       <Route path="/test/editor/:id" element={<TestEditor />} />
       <Route path="/test/play/:id" element={<PlayTest />} />
-      <Route path="/complete-profile" element={<CompleteProfile />} />
       <Route
         path="/*"
         element={
-          user ? (
-  profileExists ? (
-    <Layout user={user} logout={logout}>
-      <Routes>
-        <Route path="/" element={<Feed />} />
-        <Route path="/tests" element={<Tests />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/messages" element={<Messages />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Layout>
-  ) : (
-    <Navigate to="/complete-profile" replace />
-  )
+user ? (
+  <Layout user={user} logout={logout}>
+    <Routes>
+      <Route path="/" element={<Feed />} />
+      <Route path="/tests" element={<Tests />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
+      <Route path="/messages" element={<Messages />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  </Layout>
+) : (
+  <Navigate to="/auth" replace />
+)
 ) : (
   <Navigate to="/auth" replace />
 )
