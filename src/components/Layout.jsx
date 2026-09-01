75ba4b1933b7b5fd90fc709b8254276246f2c487
aehaaland2-lab <aehaaland2@gmail.com>
2026-09-01 15:24:17 +0500

    Fix burger menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -62,6 +62,13 @@ export default function Layout({ user, logout, children }) {
       {/* Sidebar */}
       <motion.aside
   initial={false}
+  animate={{
+    x: mobileMenu ? 0 : "-100%",
+  }}
+  transition={{
+    duration: 0.25,
+    ease: "easeOut",
+  }}
   className="
     fixed
     left-0
@@ -76,7 +83,6 @@ export default function Layout({ user, logout, children }) {
     bg-white
     p-6
     shadow-[10px_0_40px_rgba(15,23,42,0.04)]
-    -translate-x-full
     lg:translate-x-0
   "
 >
